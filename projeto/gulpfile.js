fb0f5a23fe400c09f9a2faca541145031a5944d3
Rafael Viegas <rafael.viegas@arpagus.com.br>
2017-03-31 14:42:59 -0300

    03. Lidando com latência e banda

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -1,7 +1,16 @@
-var gulp = require('gulp')
-  ,imagemin = require('gulp-imagemin')
-  ,clean = require('gulp-clean');
+var gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    htmlReplace = require('gulp-html-replace'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    cssmin = require('gulp-cssmin');
 
+gulp.task('default',['copy'], function(){
+
+    gulp.start('build-img', 'usemin');
+});
 
 gulp.task('copy', ['clean'], function() {
 	return gulp.src('src/**/*')
@@ -13,11 +22,44 @@ gulp.task('clean', function() {
 		.pipe(clean());
 });
 
-gulp.task('build-img', ['copy'], function() {
+gulp.task('build-img', function() {
 
   gulp.src('dist/img/**/*')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/img'));
 });
 
+gulp.task('build-js', function(){
+
+    gulp.src([
+        'dist/js/jquery.js',
+        'dist/js/home.js',
+        'dist/js/produto.js'
+        ])
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+        
+});
+
+gulp.task('build-html', function(){
+
+    gulp.src('dist/**/*.html')
+        .pipe(htmlReplace({
+            js: 'js/all.js'
+        }))
+        .pipe(gulp.dest('dist'));
+
+});
+
+gulp.task('usemin', function(){
+    gulp.src('dist/**/*.html')
+    .pipe(usemin({
+        'js': [uglify],
+        'css': [cssmin]
+    }))
+    .pipe(gulp.dest('dist'));
+
+
+});
 
